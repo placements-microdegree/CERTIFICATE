21acb406b2a48f748787b010b4883180e8a48dcf
habinrahman <habin936@gmail.com>
2025-09-18 10:26:08 +0530

    Update simple_test.py

--- simple_test.py
+++ simple_test.py
@@ -6,10 +6,7 @@ Simple Supabase Test
 import os
 
 # Set environment variables directly
-os.environ["SUPABASE_URL"] = "https://wyszrjhxucxblyvhrktn.supabase.co"
-os.environ["SUPABASE_KEY"] = (
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5c3pyamh4dWN4Ymx5dmhya3RuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4OTAzNzgsImV4cCI6MjA2NzQ2NjM3OH0.ZEPZIXsIVXbor8vY1uJM9VVVnody5iDJOgabbov14Xw"
-)
+
 
 print("🔍 Testing Supabase connection...")
 
